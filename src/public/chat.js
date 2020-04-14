34ce24106e5068f3269aec95de69586a9e565be1
KOseare <kdfunes@outlook.com>
2020-04-14 17:02:13 -0300

    Added system to initialize covid test and little changes on frontend

--- src/public/chat.js
+++ src/public/chat.js
@@ -10,6 +10,7 @@ sendBtn.addEventListener("click", () => {
     });
     dialogBox.innerHTML += `<div class="messageContainer"><p class="message messageUser">Yo: ${textInput.value}</p></div>`;
     dialogBox.scrollTop = dialogBox.scrollHeight;
+    textInput.value = "";
 });
 textInput.addEventListener("keyup", (event) => {
     if (event.keyCode === 13) {
@@ -19,7 +20,9 @@ textInput.addEventListener("keyup", (event) => {
 });
 
 socket.on("chat:message", (data) => {
-    dialogBox.innerHTML += `<div class="messageContainer"><p class="message messageChatbot">ChatBot: ${data.message}</p></div>`;
-    dialogBox.scrollTop = dialogBox.scrollHeight;
+    data.messages.forEach(message => {
+        dialogBox.innerHTML += `<div class="messageContainer"><p class="message messageChatbot">ChatBot: ${message}</p></div>`;
+        dialogBox.scrollTop = dialogBox.scrollHeight;
+    });
 });
 
--- src/socketConnection.js
+++ src/socketConnection.js
@@ -1,17 +1,42 @@
 const socketIo = require("socket.io");
 
+const verifyTestStart = (test, botProcess, answers) => {
+    if(botProcess.intent === "covid.sintomas"){
+        answers.push(`Si cree que pudo haber contraído COVID-19, tenemos un test para que pueda saberlo. \n ¿Quiere iniciar el test?`);            
+        test.prepare = true;
+    }
+    else if(test.prepare){
+        if(botProcess.intent === "respuesta.negativa"){
+            answers[0] = ["Bien, si no quiere hacer el test puede seguir haciendome preguntas."];
+        }
+        else if(botProcess.intent === "respuesta.afirmativa"){
+            answers[0] = ["Bien empezemos."];
+            test.start = true;
+        }
+        test.prepare = false;
+    }
+}
 
 module.exports = (server, bot) => {
     const io = socketIo(server, {origins: "*:*"});
 
     io.on("connection", (socket) => {
         console.log("New connection:", socket.id);
+        const test = {prepare: false, start: false};
 
         socket.on("chat:message", async(data) => {
             const botProcess = await bot.process("es", data.message);
             console.log(botProcess);
-            const answer = botProcess.answer || "No entiendo"; 
-            socket.emit("chat:message", {message: answer});
+            let answers = [botProcess.answer || "No entiendo, por favor reformule su pregunta."]; 
+
+            if(!test.start){
+                verifyTestStart(test, botProcess, answers);
+            }
+            if(test.start){
+                answers.push("El test todavía esta en desarrollo, por favor haga otras preguntas.");
+                test.start = false;
+            }
+            socket.emit("chat:message", {messages: answers});
         });
     });
 }
