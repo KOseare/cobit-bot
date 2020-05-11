0c8a1585141554a5b409ed25a592e4d679836465
KOseare <kdfunes@outlook.com>
2020-05-11 12:21:45 -0300

    Connected to mongo atlas

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const ip = 'mongodb://127.0.0.1:27017/cobit-19';
+//const ip = 'mongodb://127.0.0.1:27017/cobit-19';
 const db = mongoose.connection;
 
-mongoose.connect(ip, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 db.once('open', _ => {
-    console.log('Database is connected to ', ip);
+    console.log("Database is connected");
 });
 
 db.on('error', err => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const socketConnection = require("./socketConnection");
