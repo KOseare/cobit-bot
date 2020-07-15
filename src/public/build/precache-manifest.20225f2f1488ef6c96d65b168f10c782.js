9e3dbfd22b9134ff7395c8b6f0375ce66fea01f1
KOseare <kdfunes@outlook.com>
2020-07-15 20:28:37 -0300

    Fixed links again

---
+++ src/public/build/precache-manifest.20225f2f1488ef6c96d65b168f10c782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f300e7b93b3fbb9a67318d68393cdc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea27e09657b202803c60",
+    "url": "/static/css/main.d0877b0e.chunk.css"
+  },
+  {
+    "revision": "e9e28ebfd4e3b544a103",
+    "url": "/static/js/2.1bf981e8.chunk.js"
+  },
+  {
+    "revision": "b0eae4bb21377c1a3651bcefa2010b3f",
+    "url": "/static/js/2.1bf981e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea27e09657b202803c60",
+    "url": "/static/js/main.9d45da55.chunk.js"
+  },
+  {
+    "revision": "659e6029b0d2dc995968",
+    "url": "/static/js/runtime-main.c033bffa.js"
+  },
+  {
+    "revision": "55ba1929da24e0046206d2933336dc3c",
+    "url": "/static/media/Logo_CovidBot-19_opt.55ba1929.svg"
+  }
+]);
